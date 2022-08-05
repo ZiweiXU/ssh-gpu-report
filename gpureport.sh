ca2ba81665300dc312a2a727bd841670fecc02a7
Ziwei XU <sylv0527@live.com>
2022-08-05 17:16:40 +0800

    refactor with xargs and fancier wheel

--- gpureport.sh
+++ gpureport.sh
@@ -30,8 +30,6 @@ config_file=${1:-gpureport_config.sh}
 source "$config_file"
 
 ### constants and helpers
-calc() { echo - | awk "{print $1}"; }
-
 elementIn () {
   local e match="$1"
   shift
@@ -40,6 +38,88 @@ elementIn () {
 }
 
 timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+
+function cyan() {
+  if [[ -z $2 ]]; then
+    printf "\e[1;36m%s\e[0m" "$1"
+  else
+    printf "\\e[1;36m%s\\e[0m" "$1"
+  fi
+}
+
+function magenta() {
+  if [[ -n $2 ]]; then
+    printf "\e[1;35m%s\e[0m" "$1"
+  else
+    printf "\\e[1;35m%s\\e[0m" "$1"
+  fi
+}
+
+function green() {
+  if [[ -n $2 ]]; then
+    printf "\e[1;32m%s\e[0m" "$1"
+  else
+    printf "\\e[1;32m%s\\e[0m" "$1"
+  fi
+}
+
+# https://unix.stackexchange.com/questions/225179/display-spinner-while-waiting-for-some-process-to-finish
+function shutdown() {
+  tput cnorm # reset cursor
+}
+trap shutdown EXIT
+
+function cursorBack() {
+  echo -en "\033[$1D"
+}
+
+function spinner() {
+  # make sure we use non-unicode character type locale 
+  # (that way it works for any locale as long as the font supports the characters)
+  local LC_CTYPE=C
+
+  local pid=$1 # Process Id of the previous running command
+
+  case $(($RANDOM % 6)) in
+  0)
+    local spin='⠁⠂⠄⡀⢀⠠⠐⠈'
+    local charwidth=3
+    ;;
+  1)
+    local spin='-\|/'
+    local charwidth=1
+    ;;
+  2)
+    local spin='←↖↑↗→↘↓↙'
+    local charwidth=3
+    ;;
+  3)
+    local spin='▖▘▝▗'
+    local charwidth=3
+    ;;
+  4)
+    local spin='◐◓◑◒'
+    local charwidth=3
+    ;;
+  5)
+    local spin='⣾⣽⣻⢿⡿⣟⣯⣷'
+    local charwidth=3
+    ;;
+  esac
+
+  local i=0
+  tput civis # cursor invisible
+  while kill -0 $pid 2>/dev/null; do
+    local i=$(((i + $charwidth) % ${#spin}))
+    printf "%s" "${spin:$i:$charwidth}"
+
+    cursorBack 1
+    sleep .2
+  done
+  tput cnorm
+  wait $pid # capture exit code
+  return $?
+}
 
 qworker() {
     server=$1
@@ -64,108 +144,100 @@ qworker() {
     else
         echo $outcome > $TEMP_DIR/$server.servererror
     fi
+}
 
-    if test -f $TEMP_DIR/$server.gpu_info; then
-        gpu_info=$(cat $TEMP_DIR/$server.gpu_info)
-        load_avg=$(cat $TEMP_DIR/$server.load_avg)
-        
-        # truncate long / pad shorter host names into 8 characters
-        this_server=${server}
-        (( ${#this_server} > 8 )) && this_server="${this_server:0:3}..${this_server:(-3)}"
-        this_server=$(printf "%-8b" "${this_server}")
-        
-        gpu_num=$(echo "$gpu_info" | jq ".gpus | length")
-        # retrieve statistics of each gpu
-        for gpu in $(seq 0 $((gpu_num-1))) ; do
-            
-            _tmp=$(echo "$gpu_info" | jq ".gpus[$gpu] | .\"memory.total\", .\"memory.used\", .\"enforced.power.limit\", .\"power.draw\", .\"utilization.gpu\", .\"processes\" | length")
-            IFS=$'\n' read -rd '' gpu_mem_total gpu_mem_used gpu_power_limit gpu_power_draw gpu_util proc_num <<< "$_tmp"
-            gpu_mem_remain=$((gpu_mem_total-gpu_mem_used))
-            proc_usr=""
+qparser () {
+  server=$1
+  if test -f $TEMP_DIR/$server.gpu_info; then
+    printf '' > $TEMP_DIR/$server.line
+    gpu_info=$(cat $TEMP_DIR/$server.gpu_info)
+    load_avg=$(cat $TEMP_DIR/$server.load_avg)
 
-            if [[ $proc_num -gt 0 ]] ; then
-                users=()
-                for proc in $(seq 0 $(($proc_num-1))); do
-                    _tmp=$( echo $gpu_info | jq ".gpus[$gpu][\"processes\"][$proc] | .\"username\", .\"pid\"" )
-                    IFS=$'\n' read -rd '' this_usr this_pid <<< "$_tmp"
+    # truncate long / pad shorter host names into 8 characters
+    this_server=${server}
+    (( ${#this_server} > 8 )) && this_server="${this_server:0:3}..${this_server:(-3)}"
+    this_server_show_name="$(printf '%-8s' $this_server)"
+    
+    gpu_num=$(echo "$gpu_info" | jq ".gpus | length")
+    # retrieve statistics of each gpu
+    for gpu in $(seq 0 $((gpu_num-1))) ; do
+        _tmp=$(echo "$gpu_info" | jq ".gpus[$gpu] | .\"memory.total\", .\"memory.used\", .\"enforced.power.limit\", .\"power.draw\", .\"utilization.gpu\", .\"temperature.gpu\", .\"processes\" | length")
+        IFS=$'\n' read -rd '' gpu_mem_total gpu_mem_used gpu_power_limit gpu_power_draw gpu_util gpu_temp proc_num <<< "$_tmp"
+        gpu_mem_remain=$((gpu_mem_total-gpu_mem_used))
+        proc_usr=""
 
-                    if elementIn "${this_usr}" "${users[@]}" ; then continue; fi
-                    users+=("$this_usr")
-                    if [[ $this_usr = *"$USER_NAME"* ]] ; then this_usr="\\e[32m$this_usr\\e[0m"; fi
-                    
-                    proc_usr="$proc_usr""${this_usr}(${this_pid}) "
-                done
-            fi
+        if [[ $proc_num -gt 0 ]] ; then
+            # users=()
+            for proc in $(seq 0 $(($proc_num-1))); do
+                _tmp=$( echo $gpu_info | jq ".gpus[$gpu][\"processes\"][$proc] | .\"username\", .\"pid\"" )
+                IFS=$'\n' read -rd '' this_usr this_pid <<< "$_tmp"
 
-            this_server_show_name="$this_server"
-            if [[ $gpu_mem_remain -gt $RAMAIN_MEMORY_LIMIT ]]; then
-                gpu="\\e[1;36m$gpu\\e[0m"
-                this_server_show_name="\\e[1;36m$this_server_show_name\\e[0m"
-            fi
+                if [[ $this_usr = *"$USER_NAME"* ]] ; then this_usr="$(green $this_usr escape)"; fi
+                
+                proc_usr="$proc_usr""${this_usr}(${this_pid}) "
+            done
+        fi
 
-            printf "%b %b\t %5s/%-5s \t\t %-3s\t %4s/%-4s \t %s\t %b\n" \
-            "$this_server_show_name" "$gpu" "$gpu_mem_remain" "$gpu_mem_total" \
-            "$gpu_util" "$gpu_power_draw" "$gpu_power_limit" "$load_avg" "$proc_usr" >> $TEMP_DIR/$server.line
-        
-        done
-    fi
+        if [[ $gpu_mem_remain -gt $GPU_MEM_THRES ]]; then
+            gpu="$(cyan "$gpu" escape)"
+            this_server_show_name="$(cyan "$this_server_show_name" escape)"
+        fi
+
+        printf "%b %b\t %5s/%-5s \t\t %-3s\t %-3s\t %4s/%-4s \t %s\t %b\n" \
+        "$this_server_show_name" "$gpu" "$gpu_mem_remain" "$gpu_mem_total" \
+        "$gpu_util" "$gpu_temp" "$gpu_power_draw" "$gpu_power_limit" "$load_avg" "$proc_usr" >> $TEMP_DIR/$server.line
+
+    done
+  fi
 }
 
 # Start
-
 mkdir -p $TEMP_DIR
 
-RAMAIN_MEMORY_LIMIT=$((THRES*1024))
+export -f qworker qparser timeout cyan green magenta
+echo -ne 'Working... '
+printf %s\\n "${SERVER_LIST[@]}" | xargs -P ${QUERY_BATCH_SIZE} -n1 -I{} bash -c "source ${config_file}; qworker {}" &
+spinner $!
+echo 'done!'
+echo -ne 'Parsing... '
+printf %s\\n "${SERVER_LIST[@]}" | xargs -P ${QUERY_BATCH_SIZE} -n1 -I{} bash -c "source ${config_file}; qparser {}" &
+spinner $!
+echo 'done!'
+
 ssh_timeout_servers=()
 ssh_failed_servers=()
 target_failed_servers=()
 
-echo -ne "Now processing: \n"
-job_counter=0
-for server in "${SERVER_LIST[@]}" ; do
-    qworker $server &
-    echo -ne "$server "
-    job_counter=$((job_counter+1))
-    if [ $job_counter -ge $QUERY_BATCH_SIZE ] ; then 
-        wait
-        job_counter=0
-    fi
-done
-wait
-
 echo 
 echo 
-
-printf -- "------   ---\t ----------------\t ----\t ------------\t ----\t ----\n"
-printf    "SERVER   GPU\t REMAIN_M/TOTAL_M\t UTIL\t POWER/MAXPWR\t LOAD\t USER\n"
-printf -- "------   ---\t ----------------\t ----\t ------------\t ----\t ----\n"
-
+printf -- "------   ---\t ----------------\t ----\t ----\t ------------\t ----\t ---------\n"
+printf    "SERVER   GPU\t REMAIN_M/TOTAL_M\t UTIL\t TEMP\t POWER/MAXPWR\t LOAD\t USER(PID)\n"
+printf -- "------   ---\t ----------------\t ----\t ----\t ------------\t ----\t ---------\n"
+echo
 for server in "${SERVER_LIST[@]}" ; do
     # if connected and GPU info is successfully retrieved
     if test -f $TEMP_DIR/$server.line ; then
         cat $TEMP_DIR/$server.line
     # else, check the return status
     elif test -f $TEMP_DIR/$server.timeout ; then 
-        ssh_timeout_servers+=("\\e[1;35m$server\\e[0m ")
+        # ssh_timeout_servers+=("\\e[1;35m$server\\e[0m ")
+        ssh_timeout_servers+=( "$(magenta $server)"" " )
     elif test -f $TEMP_DIR/$server.sshreject ; then
-        ssh_failed_servers+=("\\e[1;35m$server\\e[0m ")
+        ssh_failed_servers+=( "$(magenta $server)"" " )
     elif test -f $TEMP_DIR/$server.servererror; then
-        target_failed_servers+=("\\e[1;35m$server\\e[0m ")
+        target_failed_servers+=( "$(magenta $server)"" " )
     else
         echo "Info file $TEMP_DIR/$server* not found. "
     fi
 done
 
-rm -r $TEMP_DIR
-
-echo
-printf "%b\n%b\n" "\\e[1;35mFailed Servers\\e[0m" "\\e[1;35m--------------\\e[0m"
-printf "%s " "SSH timeout: "
+printf "%b\n%b\n" "$(magenta 'Failed Servers')" "$(magenta '--------------')"
+printf "%s " "Timed out: "
 for fs in "${ssh_timeout_servers[@]}" ; do
     printf "%b" "$fs"
 done
 echo
-printf "%s " "SSH rejected: "
+printf "%s " "Rejected: "
 for fs in "${ssh_failed_servers[@]}" ; do
     printf "%b" "$fs"
 done
@@ -175,8 +247,7 @@ for fs in "${target_failed_servers[@]}" ; do
     printf "%b" "$fs"
 done
 
-echo 
-
 echo
 date
-# echo "$(curl -s --head http://google.com | grep ^Date: | sed 's/Date: //g')"
+
+rm -r $TEMP_DIR
