d4e5c55140161a28bea7bc1bbd5b19418dadb77c
Ziwei XU <sylv0527@live.com>
2022-08-05 17:17:35 +0800

    Update gpureport_config.sh

--- gpureport_config.sh
+++ gpureport_config.sh
@@ -6,11 +6,11 @@ SERVER_LIST=( host1 host2 host3 )
 
 # Spawn QUERY_BATCH_SIZE queries as a batch, wait until this batch is done, 
 # spawn a new batch, repeat until finish.
-QUERY_BATCH_SIZE=10
+QUERY_BATCH_SIZE=100
 
-# GPUs with remaining memory higher than THRES(GB) will be highlighted.
+# GPUs with remaining memory higher than THRES(MB) will be highlighted.
 # This value must be an integer.
-THRES=5
+GPU_MEM_THRES=5000
 
 # A username will be highlighed in the report if it contains USER_NAME
 # It has nothing to do with authentication.
